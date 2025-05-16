e86e07e74e190b7b8f21c34ce4ba9e9837f5295a
Dawit Mellese <dawitm777@gmail.com>
2025-05-17 00:13:55 +0300

    fix: update CONTRACT_ADDRESS and deployment details in DeployTipJar script

--- smart_contract/script/DeployTipJar.s.sol
+++ smart_contract/script/DeployTipJar.s.sol
@@ -27,5 +27,5 @@ contract DeployTipJar is Script {
 // forge create --rpc-url $BASE_SEPOLIA_RPC --private-key $PRIVATE_KEY src/TipJar.sol:TipJar --broadcast   --constructor-args 0x036CbD53842c5426634e7929541eC2318f3dCF7e "https://tippa.vercel.app/"
 
 // Deployer: 0xD4517B0EaddDED64bA75A65E67e2CEb1B6B0f4Fe
-// Deployed to: 0x89aAc05C68Ad149028169a040e7EE1bf720a7A2A
-// Transaction hash: 0x3f9449efdf521643cfa315a0230728bb20b7fc6eacb5c8f7d68a10387487daab
+// Deployed to: 0xc6311f0560e9B47EED500D8c8542036F044fabBF
+// Transaction hash: 0x3eb2ffbaf4599cfc5a1f3a60c97d73b8f0ec32c777158de9a01dee55c270f275
